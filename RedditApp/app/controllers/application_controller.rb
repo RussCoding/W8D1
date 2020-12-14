5fab2a0a7ef78f1554ae3d7886ef0bd8402bde75
Derek Chen <derekchen72@gmail.com>
2020-12-14 11:49:14 -0500

    application controller

--- RedditApp/app/controllers/application_controller.rb
+++ RedditApp/app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+    #
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+        current_user
+    end
+
+    def ensure_logged_in?
+        redirect_to new_session_url if current_user.nil?
+    end
+
+    def login(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout!
+        session[:session_token] = nil
+        current_user.reset_session_token!
+    end
+
+    def logged_in?
+        !!current_user
+    end
 end
